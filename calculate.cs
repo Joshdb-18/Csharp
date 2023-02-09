630daf9d0c692a1c9ae0f87d7002d13790ff2f3b
Joshdb-18 <joshuadami17@gmail.com>
2023-02-09 15:13:59 +0100

    calculate

---
+++ calculate.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("What is your monthly salary? $ ");
+        decimal salary = decimal.Parse(Console.ReadLine());
+
+        Dictionary<string, decimal> expenses = new Dictionary<string, decimal>();
+
+        Console.WriteLine("Please answer the following survey questions to calculate your daily expenses:");
+
+        expenses["Food"] = decimal.Parse(Console.ReadLine("How much do you spend on food per month? $ "));
+        expenses["Housing"] = decimal.Parse(Console.ReadLine("How much do you spend on housing per month? $ "));
+        expenses["Transportation"] = decimal.Parse(Console.ReadLine("How much do you spend on transportation per month? $ "));
+        expenses["Utilities"] = decimal.Parse(Console.ReadLine("How much do you spend on utilities (e.g. electricity, water) per month? $ "));
+        expenses["Entertainment"] = decimal.Parse(Console.ReadLine("How much do you spend on entertainment (e.g. movies, hobbies) per month? $ "));
+	expenses["Unforseen_circumstances"] = decimal.Parse(Console.ReadLine("How much do you intend to spend on Unforseen circumstances? $"));
+        
+	decimal totalExpenses = 0;
+        foreach (var expense in expenses.Values)
+        {
+            totalExpenses += expense;
+        }
+
+        decimal dailyExpenses = totalExpenses / 30;
+        decimal dailyBudget = salary / 30;
+
+        if (dailyBudget >= dailyExpenses)
+        {
+            Console.WriteLine($"Your daily budget is ${dailyBudget:0.00}.");
+            Console.WriteLine("You need to spend the following amount on each item each day:");
+            foreach (var expense in expenses)
+            {
+                Console.WriteLine($"{expense.Key}: ${expense.Value / 30:0.00}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("You are overspending.");
+        }
+    }
+}
+
